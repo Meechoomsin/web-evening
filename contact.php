d2591beb192830a1ddfbac8152746bea80670d07
Meechoomsin <matornpro@gmail.com>
2022-06-29 19:24:13 +0700

    first commit

---
+++ contact.php
@@ -0,0 +1,9 @@
+<?php
+echo "<h1>ติดต่อเรา</h1>";
+include "navbar.php";
+
+// echo "<a href='contact.php'>ติดต่อเรา</a>";
+// echo "<a href='about.php'>เกี่ยวกับเรา</a>";
+// echo "<a href='index.php'หนัาหลัก</a>";
+
+?>
---
+++ navbar.php
@@ -0,0 +1,3 @@
+<a href="index.php">หน้าหลัก</a>
+<a href="about.php">เกี่ยวกับ</a>
+<a href="contact.php">ติดต่อ</a>
